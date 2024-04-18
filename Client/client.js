792d921baca5a970aee7d47478eaffbca33d11b7
ReginaOchoaGispert <0244214@up.edu.mx>
2024-04-18 13:09:36 -0600

    Comunicacion
    Comunicación entre los dos clientes a través del servidor

--- Client/client.js
+++ Client/client.js
@@ -4,20 +4,102 @@ const net = require('net');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = 5060;
+const PORT2 = 5001;
 const HOST = '127.0.0.1';
-let turno = 0; // Utiliza let en lugar de var para reasignar el valor de turno
+let turno = 0;
+var meToca = false;
+var flag = false;
+let tablero = Array.from({ length: 6 }, () => Array.from({ length: 7 }, () => 0));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('/socket.io', express.static(path.join(__dirname, 'node_modules/socket.io/client-dist')));
 
-// Página inicial con formulario de inicio de sesión
+function preguntarTurno() {
+    if(!meToca){
+        const client = new net.Socket();
+
+        client.connect(PORT, HOST, () => {
+            //console.log(`${turno}:Me toca?`);
+            client.write(`${turno}:Me toca?`);
+        });
+
+        client.on('data', (data) => {
+            const respuesta = data.toString();
+            console.log(`Respuesta del servidor: ${respuesta}`);
+
+            // Dividir la respuesta en dos partes usando el delimitador ":"
+            const partes = respuesta.split(':');
+
+            // partes[0] contendrÃ¡ "SI" y partes[1] contendrÃ¡ "Tu turno"
+            const primeraParte = partes[0];
+            const segundaParte = partes[1];
+
+            meToca = primeraParte === "Si";
+            if(primeraParte === "Si"){
+                tablero = JSON.parse(segundaParte);
+                console.log("drtfgyvhbujnkml: ");
+                for (let fila of tablero) {
+                    console.log(fila.join(' '));
+                }
+            }
+            
+            client.destroy();
+        });
+
+        client.on('close', () => {
+            console.log('ConexiÃ³n cerrada');
+        });
+
+        client.on('error', (err) => {
+            console.error(`Error: ${err}`);
+        });
+    }
+}
+
+setInterval(preguntarTurno, 4000);
+
 app.get('/', (req, res) => {
-    res.render('index', { message: '' }); // Renderiza la vista index con un mensaje vacío
+    res.render('index', { message: '' });
 });
 
-// Ruta para procesar el formulario de inicio de sesión
+app.post('/send', (req, res) => {
+    const { col } = req.body;
+    console.log('Mensaje recibido del cliente: ', col);
+
+    // Encuentra la primera fila disponible en la columna especificada
+    let fila = 5;
+    while (fila >= 0 && tablero[fila][col] !== 0) {
+        fila--;
+        
+    }
+    console.log('Fila: ', fila);
+    if (fila >= 0) {
+        tablero[fila][col] = turno;
+        console.log('Tablero actualizado:', tablero);
+    } else {
+        console.log('Columna llena, no se puede colocar la ficha');
+    }
+
+    const client = new net.Socket();
+    client.connect(PORT, HOST, () => {
+        //console.log('Conexion establecida con el servidor');
+        let mensaje = turno.toString() + ":" + JSON.stringify(tablero);
+        console.log(`Mensaje enviado: ${mensaje}`);
+        client.write(mensaje);
+    });
+    client.on('data', (data) => {
+        console.log(`Respuesta del servidor: ${data}`);
+        client.destroy();
+    });
+
+    res.json({ message: 'Columna recibida correctamente' });
+    meToca = false;
+});
+
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -25,7 +107,7 @@ app.post('/login', (req, res) => {
     const client = new net.Socket();
 
     client.connect(PORT, HOST, () => {
-        console.log('Conexión establecida con el servidor');
+        //console.log('Conexion establecida con el servidor');
 
         const credentials = `${username}:${password}`;
         client.write(credentials);
@@ -33,15 +115,14 @@ app.post('/login', (req, res) => {
 
     client.on('data', (data) => {
         console.log(`Respuesta del servidor: ${data}`);
-        const response = data.toString();
-        const partes = response.split(':');
-        turno = parseInt(partes[0]); // Convierte turno a número entero
+        if(!flag){
+            turno = data.toString();
+            flag = true;
+        }
 
-        if (turno !== 0) {
-            // Redirige a la página de juego de Conecta 4 si turno es diferente de 0
+        if (turno != "0") {
             res.redirect('/juego');
         } else {
-            // Renderiza la vista index con un mensaje de usuario no encontrado si turno es 0
             res.render('index', { message: 'Usuario no encontrado' });
         }
 
@@ -49,7 +130,7 @@ app.post('/login', (req, res) => {
     });
 
     client.on('close', () => {
-        console.log('Conexión cerrada');
+        console.log('Conexiï¿½n cerrada');
     });
 
     client.on('error', (err) => {
@@ -58,16 +139,16 @@ app.post('/login', (req, res) => {
     });
 });
 
-// Ruta para la página del juego de Conecta 4
+// Ruta para la pï¿½gina del juego de Conecta 4
 app.get('/juego', (req, res) => {
-    res.render('juego'); // Renderiza la vista del juego de Conecta 4
+    res.render('juego', { meToca: meToca, tablero: tablero }); // Renderiza la vista del juego de Conecta 4
 });
 
 // Ruta para manejar cualquier otra solicitud no encontrada
 app.use((req, res) => {
-    res.status(404).send('Página no encontrada');
+    res.status(404).send('Pï¿½gina no encontrada');
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor Express iniciado en http://localhost:${PORT}`);
+app.listen(PORT2, () => {
+    console.log(`Servidor Express iniciado en http://localhost:${PORT2}`);
 });
--- server/server.c
+++ server/server.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <winsock2.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <stdbool.h>
 
 #define MAX_CLIENTS 10
@@ -29,26 +30,20 @@ int authenticate(char *username, char *password) {
     return 0;
 }
 
+int stringToInt(char* str) {
+    return (str != NULL && str[0] != '\0') ? atoi(str) : 0;
+}
+
 int main() {
-    WSADATA wsa_data;
-    bool estado = false;
-    int cont = 2;
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
-        printf("Error al inicializar Winsock\n");
-        return 1;
-    }
-
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
-    int addr_len = sizeof(struct sockaddr_in);
+    socklen_t addr_len = sizeof(struct sockaddr_in);
     char buffer[BUFFER_SIZE];
 
     // Crear el socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
         perror("Error al crear el socket");
-        WSACleanup();
         exit(EXIT_FAILURE);
     }
 
@@ -56,25 +51,37 @@ int main() {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(3000);
+    server_addr.sin_port = htons(5060);
 
     // Vincular el socket al puerto
     if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Error al vincular el socket");
-        closesocket(server_fd);
-        WSACleanup();
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
 
     // Escuchar conexiones entrantes
     if (listen(server_fd, MAX_CLIENTS) < 0) {
         perror("Error al escuchar");
-        closesocket(server_fd);
-        WSACleanup();
+        close(server_fd);
         exit(EXIT_FAILURE);
     }
 
     printf("Servidor iniciado. Esperando conexiones...\n");
+
+    bool autentificados = false;
+    int cont = 2;
+    char *leTocaA = "1";
+    int tablero[6][7];
+    int tableroMensaje[6][7]; // Inicializar la matriz con ceros
+    memset(tableroMensaje, 8, sizeof(tablero)); // Asegurarse de que todos los elementos sean 0
+    
+    // Inicializando el tablero con ceros
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 7; j++) {
+            tablero[i][j] = 0;
+        }
+    }
 
     while (1) {
         // Aceptar una conexión entrante
@@ -83,20 +90,20 @@ int main() {
             perror("Error al aceptar la conexión");
             continue;
         }
-
         printf("Nueva conexión aceptada desde %s:%d\n",
                inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
         // Recibir las credenciales del cliente
         ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+
         if (bytes_received <= 0) {
             perror("Error al recibir datos");
-            closesocket(client_fd);
+            close(client_fd);
             continue;
         }
         buffer[bytes_received] = '\0';
-        
-        if(!estado){
+        printf("Recibi: %s\n", buffer);
+        if(!autentificados){
             // Separar el nombre de usuario y la contraseña
             char *username = strtok(buffer, ":");
             char *password = strtok(NULL, ":");
@@ -105,34 +112,115 @@ int main() {
                 printf("Formato de credenciales inválido\n");
                 const char *response = "Formato de credenciales inválido";
                 send(client_fd, response, strlen(response), 0);
-                closesocket(client_fd);
+                close(client_fd);
                 continue;
             }
 
             // Autenticar al usuario
-            if (authenticate(username, password)) {
+            if(strcmp(username, "2") == 0){
+                printf("Envie: No\n");
+                const char *response = "No";
+                send(client_fd, response, strlen(response), 0);
+            }
+            else if (authenticate(username, password)) {
                 printf("Usuario autenticado: %s\n", username);
                 char numero_str[20];
                 sprintf(numero_str, "%d", cont);
-                cont--;    
+                cont--;
+                if(cont <= 0){
+                    autentificados = true;
+                }
                 send(client_fd, numero_str, strlen(numero_str), 0);
             } else {
                 printf("Credenciales inválidas para: %s\n", username);
                 const char *response = "0";
                 send(client_fd, response, strlen(response), 0);
             }
-
-            if(cont <= 0 ){
-                estado = true;
-
-            }
-
         }
+        else{
+            char *jugador = strtok(buffer, ":");
+            char *mensaje = strtok(NULL, ":");
+            printf("Jugador: %s\n", jugador);
+            printf("le Toca a: %s\n", leTocaA);
+            printf("Mensaje: %s\n", mensaje);
+            if(strcmp(jugador, leTocaA) != 0 && strcmp(mensaje, "Me toca?") == 0){
+                //printf("Envie: No, pregunto: %s\n", jugador);
+                const char *response = "No";
+                send(client_fd, response, strlen(response), 0);
+            }
+            else if(strcmp(jugador, leTocaA) == 0 && strcmp(mensaje, "Me toca?") == 0){
+                char tableroStr[256];
+                sprintf(tableroStr, "[[%d, %d, %d, %d, %d, %d, %d],[%d, %d, %d, %d, %d, %d, %d],[%d, %d, %d, %d, %d, %d, %d],[%d, %d, %d, %d, %d, %d, %d],[%d, %d, %d, %d, %d, %d, %d],[%d, %d, %d, %d, %d, %d, %d]]",
+                        tablero[0][0], tablero[0][1], tablero[0][2], tablero[0][3], tablero[0][4], tablero[0][5], tablero[0][6],
+                        tablero[1][0], tablero[1][1], tablero[1][2], tablero[1][3], tablero[1][4], tablero[1][5], tablero[1][6],
+                        tablero[2][0], tablero[2][1], tablero[2][2], tablero[2][3], tablero[2][4], tablero[2][5], tablero[2][6],
+                        tablero[3][0], tablero[3][1], tablero[3][2], tablero[3][3], tablero[3][4], tablero[3][5], tablero[3][6],
+                        tablero[4][0], tablero[4][1], tablero[4][2], tablero[4][3], tablero[4][4], tablero[4][5], tablero[4][6],
+                        tablero[5][0], tablero[5][1], tablero[5][2], tablero[5][3], tablero[5][4], tablero[5][5], tablero[5][6]);
+                char response[strlen(tableroStr) + 4];
+                sprintf(response, "Si:%s", tableroStr);
+                printf("Envie: %s\n", response);
+                send(client_fd, response, strlen(response), 0);
+                printf("Si se enviooooooooooooooooooooooooooooooooooo\n");
+            }
+            else if(strcmp(jugador, leTocaA) == 0){
+                //Lo guarda
+                printf("va a guardar\n");
+                /*
+                char *fila;
+                char *element;
+                int i = 0, j;
 
-        closesocket(client_fd);
+                do {
+                    fila = strtok(mensaje, "["); // Obtener la primera fila
+                    printf("Siguiente fila: %s\n", fila);
+                    j = 0;
+                    do{
+                        element = strtok(fila, ",");
+                        printf("elemento: %s\n", element);
+                        tableroMensaje[i][j] = stringToInt(element); // Convertir la cadena a entero y almacenarla en la matriz
+                        j++;
+                        element = strtok(NULL, ","); // Obtener el siguiente elemento de la fila
+                    }
+                    while (element != NULL); 
+                    printf("%s\n",fila);
+                    i++;
+                    fila = strtok(NULL, "["); // Obtener la siguiente fila
+                } while (fila != NULL);*/
+                int it=0, fil=0, col=0;
+                while(mensaje[it]!='\0'){
+                    if(mensaje[it]==']'){
+                        col++;
+                        fil=0;
+                    }
+                    if(mensaje[it]>='0'&&mensaje[it]<='9'){
+                        tablero[col][fil]=(int)(mensaje[it]-'0');
+                        fil++;
+                    }
+                    it++;
+                }
+                // Imprimiendo el tablero
+                printf("Tablero guardado: \n");
+                for (int i = 0; i < 6; i++) {
+                    for (int j = 0; j < 7; j++) {
+                        printf("%d ", tablero[i][j]);
+                    }
+                    printf("\n");  // Salto de línea después de cada fila
+                }
+                //logica para ver si alguien ya gano
+                printf("Turno cambiado\n");
+                if(leTocaA == "1"){
+                    leTocaA = "2";
+                }
+                else{
+                    leTocaA = "1";
+                }
+            }
+        }
+        printf("---------------------------------------\n");
+        close(client_fd);
     }
 
-    closesocket(server_fd);
-    WSACleanup();
+    close(server_fd);
     return 0;
 }
